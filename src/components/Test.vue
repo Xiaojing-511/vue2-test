18e9036879250b0c4d8fb77d87e2a8ad8b42971e
zhangjingjing <zhangjingjing11@xiaomi.com>
2021-08-26 19:32:23 +0800

    [add] 算法

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-08-24 17:09:57
- * @LastEditTime: 2021-08-26 14:50:39
+ * @LastEditTime: 2021-08-26 19:29:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue2-test/src/components/Test.vue
@@ -17,7 +17,8 @@
 // import '../js/new'
 // import '../js/promise'
 // import '../js/throttle'
-import '../js/继承'
+// import '../js/继承'
+import '../算法/字符串-子串数量'
 export default {
     
 }
---
+++ src/算法/字符串-子串数量.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-26 18:14:26
+ * @LastEditTime: 2021-08-26 19:31:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue2-test/src/算法/字符串-子串数量.js
+ */
+
+// 统计规定开头和结尾的子串数
+function totalCounter(str,start,end){
+    let count = 0,
+    aCount = 0;
+    for(var i = 0; i < str.length; i++){
+        if(str[i] === start) {
+            aCount++;
+        }else if(str[i] === end){
+            count += aCount;
+        }
+    }
+    if(start === end) return aCount*(aCount - 1)/2;
+    return count;
+}
+console.log(totalCounter('anhnhjbabb','a','b'));
+console.log(totalCounter('anhnhjbabb','a','a'));
